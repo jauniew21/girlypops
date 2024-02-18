ceb92b4a2c83d8bf1e7613133b972d6d0d236978
Jaunie Williams <jauniew21@gmail.com>
2024-02-17 17:14:03 -0700

    Vik is so good

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React = require("react")
 
 export default function RootLayout({
     children,
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React = require("react")
 
 export default function Page() {
     return <h1>Hello, Next.js!</h1>
